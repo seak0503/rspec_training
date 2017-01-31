bed9e797c89c2521ca6a30cbd369d406b48d0da3
shyamahira <shyamahira@bbtower.co.jp>
2017-01-31 23:18:49 +0900

    example の再利用: shared_examples と it_behaves_likeまで完了

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,20 +2,31 @@ require 'spec_helper'
 
 RSpec.describe User do
   describe '#greet' do
-    before do
-      @params = { name: 'たろう' }
+    let(:user) { User.new(name: 'たろう', age: age) }
+    subject { user.greet }
+
+    shared_examples '子どものあいさつ' do
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    context '12歳以下の場合' do
-      it 'ひらがなで答えること' do
-        user = User.new(@params.merge(age: 12))
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子どものあいさつ'
     end
-    context '13歳以上の場合' do
-      it '漢字で答えること' do
-        user = User.new(@params.merge(age: 13))
-        expect(user.greet).to eq '僕はたろうです。'
-      end
+    context '12歳の場合' do
+      let(:age) { 12 }
+      it_behaves_like '子どものあいさつ'
+    end
+
+    shared_examples '大人のあいさつ' do
+      it { is_expected.to eq '僕はたろうです。' }
+    end
+    context '13歳の場合' do
+      let(:age) { 13 }
+      it_behaves_like '大人のあいさつ'
+    end
+    context '100歳の場合' do
+      let(:age) { 100 }
+      it_behaves_like '大人のあいさつ'
     end
   end
 end
